cce59aa24bb3cdfc510fe550b02f861f535a40c4
travis <travis@travis-ci.org>
2018-12-08 22:48:26 +0800

    share

--- pages/guodu/guodu.js
+++ pages/guodu/guodu.js
@@ -3,5 +3,11 @@ const app = getApp()
 Page({
   data: {
     baseUrl: app.globalData.baseUrl
+  },
+  bindtap(){
+    console.log(4444)
+    wx.navigateTo({
+      url: '../page/page'
+    })
   }
 })
