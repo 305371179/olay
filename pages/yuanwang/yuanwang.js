0acdbff9dab902736bcfb2c39dd952d2e0d87bf6
travis <travis@travis-ci.org>
2018-12-10 21:41:15 +0800

    a

--- pages/yuanwang/yuanwang.js
+++ pages/yuanwang/yuanwang.js
@@ -1,22 +1,31 @@
 const app = getApp()
-
+const data=[
+  '齐齐整整\n家人安康',
+  '遇见对的人\n成为更好的自己',
+  '世界会为我让路\n迎着梦想前行',
+  '期待的突破会到来\n都会得到认可和回报\n付出的努力',
+  ''
+]
 Page({
   data: {
     baseUrl: app.globalData.baseUrl,
-    current: 0,
-    value: '家人安康\n整整齐齐',
+    current: 4,
+    value: '',
     fontSize: 80,
     width:80,
     values: [],
     labelShow:true,
+    focus: false
   },
   onLoad(options){
     if(options.type!==undefined)
     this.setData({
       current:options.type
     })
+    const value = data[this.data.current]
     this.setData({
-      values: this.data.value.split('\n')
+      value: value,
+      values: value.split('\n')
     })
     this.setFontSize()
   },
@@ -32,18 +41,22 @@ Page({
   },
   bindTextAreaBlur(){
     this.setData({
-      labelShow: true
+      labelShow: true,
+      focus: false
     })
   },
   bindTextAreaFocus(){
     this.setData({
-      labelShow: false
+      labelShow: false,
+      focus: true
     })
   },
   setFontSize(){
     const length = this.data.value.length
     let size = 80// parseInt(20/length*80)
-    if(length<10){
+    if(length<5){
+      size = 120
+    }else if(length<10){
       size=80
     }else if(length<15){
       size = 60
@@ -60,6 +73,7 @@ Page({
       value: e.detail.value,
       values: e.detail.value.split('\n')
     })
+    this.setFontSize()
     // console.log(e.detail.value.split('\n'))
   }
 })
