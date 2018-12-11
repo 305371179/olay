4ba2e09c7fac079a50dd54bcc4f472ec6952d28f
travis <travis@travis-ci.org>
2018-12-11 14:21:51 +0800

    friendIn

--- pages/friendIn/friendIn.js
+++ pages/friendIn/friendIn.js
@@ -8,15 +8,29 @@ Page({
     nickName:'',
     hasUserInfo: false,
     show: false,
-    current:4,
-    value: 'adfd',
+    id: '',
+    current:4,//愿望的类型
+    value: 'adfd',//愿望的文本
     values: ['整整齐齐','全家安康'],
     labelShow:true,
     //旅行助力
     lvxingText:16,
     wuzheText:16,
     yiyuanText:16,
-    zans:[]
+    zans:[],
+    //点赞数
+    zanCount: 0,
+    defaultHead: 'background-image:url('+app.globalData.baseUrl+'friendIn/zanfirst.png'+')',
+    //是否可以点赞
+    isCanZan: true,
+    //进度条的宽度
+    progressWidth: 0,
+    fullWidth: 563,
+    goldStar: app.globalData.baseUrl+'friendIn/progress-star-gold.png',
+    silverStar: app.globalData.baseUrl+'friendIn/progress-star-silver.png',
+    zan15: app.globalData.baseUrl+'friendIn/progress-star-silver.png',
+    zan30: app.globalData.baseUrl+'friendIn/progress-star-silver.png',
+    zan100: app.globalData.baseUrl+'friendIn/progress-star-silver.png',
   },
   setFontSize(){
     const length = this.data.value.length
@@ -37,6 +51,11 @@ Page({
     })
   },
   onLoad(options){
+    if(options.id){
+      this.setData({
+        id:options.id
+      })
+    }
     if(options.type!==undefined){
       const value = options.value
       this.setData({
@@ -47,11 +66,36 @@ Page({
     }
     this.setFontSize()
     this.setZans()
+    this.setProgress(100)
+  },
+  setProgress(zanCount){
+    let zan15 = this.data.silverStar
+    let zan30 = this.data.silverStar
+    let zan100 = this.data.silverStar
+    if(zanCount>=15){
+      zan15 = this.data.goldStar
+    }
+    if(zanCount>=30){
+      zan30 = this.data.goldStar
+    }
+    if(zanCount>=100){
+      zan100 = this.data.goldStar
+    }
+    this.setData({
+      zanCount: zanCount,
+      progressWidth: Math.round(zanCount/100*this.data.fullWidth),
+      zan15,
+      zan30,
+      zan100,
+    })
   },
   setZans(){
     let zans = []
     for (let i = 0; i < 40; i++) {
-      zans.push(i)
+      zans.push({
+        head:'',
+        background : this.data.defaultHead
+      })
     }
     this.setData({
       zans: zans
@@ -67,6 +111,18 @@ Page({
   },
   //点赞
   zan(){
-
+    if(!this.data.isCanZan){
+      return
+    }
+    this.data.isCanZan = false
+    this.data.zans.pop()
+    this.data.zans.splice(0,0,{
+      //模拟头像
+      head:app.globalData.baseUrl+'friendIn/head.png',
+      background: ''
+    })
+    this.setData({
+      zans: this.data.zans
+    })
   }
 })
